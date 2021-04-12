8fc703ba1df466baaef0bfb982b99f5c6af216dd
sharath <Bharath.s@goodworklabs.com>
2021-04-12 15:48:18 +0530

    initailize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3002;
+// app.get('/', function (req, res){
+// 	res.send('hellow express!');
+// });
+
+var middleware = {
+	requireAuthentication:function(req, res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger : function( req, res, next){
+		console.log('request:'+ new Date().toStringx() + ' ' + req.method +' '+ req.originalUrl);
+		next();
+	}
+};
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication ,function (req, res){
+	res.send(' content of about!');
+});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function (){
+	console.log('express  server is started! using port no is '+ PORT +'!');
+}); 
